e3e12f038e4d6cd32b44d6f8e70b477a751e43cd
RiyaP <riya.pathe@cumminscollege.in>
2023-08-07 17:29:37 +0530

    done with login

--- react-app/src/Routes/Home.js
+++ react-app/src/Routes/Home.js
@@ -1,9 +1,31 @@
-import React from 'react'
+// Home.js
+// Home.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'D:/arrakis_v2/react-app/src/App.css';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+       
+      <div className="card">
+      
+      
+      
+      
+      <div className="button-container">
+      <h1>Welcome to Our Website</h1>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+    </div> 
+    </div>
+  );
+};
 
-export default Home
+export default Home;
+
--- react-app/src/Routes/Login.js
+++ react-app/src/Routes/Login.js
@@ -1,9 +1,42 @@
-import React from 'react'
+
+
+import React, { useState } from 'react';
+import 'D:/arrakis_v2/react-app/src/styles/Login.css';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-export default Login
+  const handleLogin = () => {
+    if (username === 'yourUsername' && password === 'yourPassword') {
+      // Successful login logic
+      setError('');
+      alert('Login successful!');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
+  return (
+    <div className="login">
+      <h2 color='black'>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
