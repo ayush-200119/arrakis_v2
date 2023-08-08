8b25033337c0608f1bff10405d1308c5d3f16f99
RiyaP <riya.pathe@cumminscollege.in>
2023-08-08 16:31:43 +0530

    user

--- react-app/src/Routes/User/UserLanding.js
+++ react-app/src/Routes/User/UserLanding.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import { default as React, useState } from 'react';
+import '../../App.css';
 import AuthService from '../../services/auth.service';
-import '../../App.css'
-=======
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import 'D:/arrakis_v2/react-app/src/App.css';
->>>>>>> da1afc24c3a4311cf78a4f7cc01feecabc3c28f2
 
 const tradesData = [
   { id: 1, bookId: 'B001', quantity: 100, status: 'Active', price: 105, buyOrSell: 'Buy', tradeDate: '2023-08-10', settlementDate: '2023-08-12', counterparty: 'Counterparty A', issuer: 'Issuer X', maturity: '2025-12-31' },
@@ -15,7 +9,6 @@ const tradesData = [
 ];
 
 const UserLanding = () => {
-<<<<<<< HEAD
 const user = AuthService.getCurrentUser();
 console.log("user is : " , user)
 const [bonds, setBonds] = useState(bondsData);
@@ -34,6 +27,7 @@ const handleTypeChange = (event) => {
 };
 
 return (
+  
   <div className="bond-container">
     <h1>Securities</h1>
     <div className="filter-container">
@@ -44,46 +38,7 @@ return (
         <option value="corporate">Corporate</option>
         <option value="municipal">Municipal</option>
       </select>
-=======
-
-  const [trades] = useState(tradesData);
-
-  return (
-    <div className="dashboard">
-      <h1>Trade Management Dashboard</h1>
-      <table className="trade-table">
-        <thead>
-          <tr>
-            <th>Book ID</th>
-            <th>Quantity</th>
-            <th>Status</th>
-            <th>Price</th>
-            <th>Buy/Sell</th>
-            <th>Trade Date</th>
-            <th>Settlement Date</th>
-            <th>Counterparty</th>
-            <th>Issuer</th>
-            <th>Maturity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {trades.map(trade => (
-            <tr key={trade.id}>
-              <td><Link to={`/SecurityCard/${trade.id}`}>{trade.bookId}</Link></td>
-              <td>{trade.quantity}</td>
-              <td>{trade.status}</td>
-              <td>{trade.price}</td>
-              <td>{trade.buyOrSell}</td>
-              <td>{trade.tradeDate}</td>
-              <td>{trade.settlementDate}</td>
-              <td>{trade.counterparty}</td>
-              <td>{trade.issuer}</td>
-              <td>{trade.maturity}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
->>>>>>> da1afc24c3a4311cf78a4f7cc01feecabc3c28f2
+    </div>
     </div>
   );
 };
